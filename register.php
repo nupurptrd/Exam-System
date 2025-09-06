b7a565fd2e3f6a962ea6a5d29382a81eaa697f61
nupur <nupurpanchal@gmail.com>
2025-09-06 20:08:27 +0530

    new pc

--- register.php
+++ register.php
@@ -26,6 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body class="bg-gray-100">
     <main class="container mx-auto p-4" >
